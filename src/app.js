c27e1573d56194e1c9299c204f6ffb4881aded1f
Lina María <66230572+linamaria126@users.noreply.github.com>
2023-10-16 17:18:51 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.ramdom() * 10;
+
+  console.log("Hello Rigo from the console!" + randomNumber);
 };
